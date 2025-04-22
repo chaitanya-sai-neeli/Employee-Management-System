d27f0695b03ae8615d60aecd327d55c106c2db3f
chaitanya-sai-neeli <chaitanyasaichaitu123@gmail.com>
2025-04-22 22:53:05 +0530

    Header comp added

---
+++ src/components/Others/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="flex items-end justify-between">
+      <h1 className="text-2xl font-medium">
+        Hello <br />
+        <span className="text-3xl font-semibold">Chaitanya 👋</span>
+      </h1>
+      <button className="text-lg font-medium px-5 py-2 text-white bg-red-600 rounded-sm">
+        Log Out
+      </button>
+    </div>
+  );
+};
+
+export default Header;
