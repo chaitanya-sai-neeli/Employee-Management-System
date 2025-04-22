afac23583faf45115fe1377a23ed9fb2f2504acb
chaitanya-sai-neeli <chaitanyasaichaitu123@gmail.com>
2025-04-22 23:38:20 +0530

    Added taskList component as scrolling along X axis

---
+++ src/components/TaskList/TasksListCards.jsx
@@ -0,0 +1,125 @@
+import React from "react";
+
+const TasksListCards = () => {
+  return (
+    <div
+      id="taskList"
+      className="mt-10 h-[55%] w-full p-5 flex overflow-x-auto items-center justify-start gap-5 flex-nowrap rounded-xl"
+    >
+      <div className="h-full w-[300px] flex-shrink-0 bg-yellow-400 rounded-xl">
+        <div className="p-5 flex justify-between items-center">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded text-white">
+            High
+          </h3>
+          <h4 className="text-sm ">20 Feb 2024</h4>
+        </div>
+        <div className="px-5 py-2">
+          <h2 className="text-2xl font-semibold">Make a YouTube Video</h2>
+          <p className="text-sm mt-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo,
+            molestiae! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Explicabo, molestiae!
+          </p>
+        </div>
+      </div>
+      <div className="h-full w-[300px] flex-shrink-0 bg-red-400 rounded-xl">
+        <div className="p-5 flex justify-between items-center">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded text-white">
+            High
+          </h3>
+          <h4 className="text-sm ">20 Feb 2024</h4>
+        </div>
+        <div className="px-5 py-2">
+          <h2 className="text-2xl font-semibold">Make a YouTube Video</h2>
+          <p className="text-sm mt-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo,
+            molestiae! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Explicabo, molestiae!
+          </p>
+        </div>
+      </div>
+      <div className="h-full w-[300px] flex-shrink-0 bg-green-400 rounded-xl">
+        <div className="p-5 flex justify-between items-center">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded text-white">
+            High
+          </h3>
+          <h4 className="text-sm ">20 Feb 2024</h4>
+        </div>
+        <div className="px-5 py-2">
+          <h2 className="text-2xl font-semibold">Make a YouTube Video</h2>
+          <p className="text-sm mt-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo,
+            molestiae! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Explicabo, molestiae!
+          </p>
+        </div>
+      </div>
+      <div className="h-full w-[300px] flex-shrink-0 bg-blue-400 rounded-xl">
+        <div className="p-5 flex justify-between items-center">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded text-white">
+            High
+          </h3>
+          <h4 className="text-sm ">20 Feb 2024</h4>
+        </div>
+        <div className="px-5 py-2">
+          <h2 className="text-2xl font-semibold">Make a YouTube Video</h2>
+          <p className="text-sm mt-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo,
+            molestiae! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Explicabo, molestiae!
+          </p>
+        </div>
+      </div>
+      <div className="h-full w-[300px] flex-shrink-0 bg-yellow-400 rounded-xl">
+        <div className="p-5 flex justify-between items-center">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded text-white">
+            High
+          </h3>
+          <h4 className="text-sm ">20 Feb 2024</h4>
+        </div>
+        <div className="px-5 py-2">
+          <h2 className="text-2xl font-semibold">Make a YouTube Video</h2>
+          <p className="text-sm mt-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo,
+            molestiae! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Explicabo, molestiae!
+          </p>
+        </div>
+      </div>
+      <div className="h-full w-[300px] flex-shrink-0 bg-green-400 rounded-xl">
+        <div className="p-5 flex justify-between items-center">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded text-white">
+            High
+          </h3>
+          <h4 className="text-sm ">20 Feb 2024</h4>
+        </div>
+        <div className="px-5 py-2">
+          <h2 className="text-2xl font-semibold">Make a YouTube Video</h2>
+          <p className="text-sm mt-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo,
+            molestiae! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Explicabo, molestiae!
+          </p>
+        </div>
+      </div>
+      <div className="h-full w-[300px] flex-shrink-0 bg-red-400 rounded-xl">
+        <div className="p-5 flex justify-between items-center">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded text-white">
+            High
+          </h3>
+          <h4 className="text-sm ">20 Feb 2024</h4>
+        </div>
+        <div className="px-5 py-2">
+          <h2 className="text-2xl font-semibold">Make a YouTube Video</h2>
+          <p className="text-sm mt-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo,
+            molestiae! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Explicabo, molestiae!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TasksListCards;
