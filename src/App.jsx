d9ab7cf2d07ffa733992c3b152ec006ab12abc85
chaitanya-sai-neeli <chaitanyasaichaitu123@gmail.com>
2025-04-22 23:37:24 +0530

    Updated the layput in appropriate component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import Login from "./components/Auth/Login";
 import EmployeeDashboard from "./components/Dashboard/EmployeeDashboard";
-import TaskTypeAndNumber from "./components/Others/TaskTypeAndNumber";
 
 const App = () => {
   return (
     <>
       {/* <Login /> */}
       <EmployeeDashboard />
-      <TaskTypeAndNumber />
     </>
   );
 };
--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Header from "../Others/Header";
+import TaskTypeAndNumber from "../Others/TaskTypeAndNumber";
+import TasksListCards from "../TaskList/TasksListCards";
 
 const EmployeeDashboard = () => {
   return (
-    <div className="p-10 bg-[#1c1c1c] screen">
+    <div className="p-10 bg-[#1c1c1c] h-screen">
       <Header />
+      <TaskTypeAndNumber />
+      <TasksListCards />
     </div>
   );
 };
