1d462f29a1feea5978731b1d37864c82bd971d93
chaitanya-sai-neeli <chaitanyasaichaitu123@gmail.com>
2025-04-23 12:05:27 +0530

    Added AdminDashboard and create task components

--- src/components/Others/Header.jsx
+++ src/components/Others/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     <div className="flex items-end justify-between">
       <h1 className="text-2xl font-medium">
         Hello <br />
-        <span className="text-3xl font-semibold">Chaitanya 👋</span>
+        <span className="text-3xl font-semibold">Chaitanya👋</span>
       </h1>
       <button className="text-lg font-medium px-5 py-2 text-white bg-red-600 rounded-sm">
         Log Out
