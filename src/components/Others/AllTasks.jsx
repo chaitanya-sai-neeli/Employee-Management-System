bfc8c7ebb9c680af8a985668dfa6c32a38b2a54a
chaitanya-sai-neeli <chaitanyasaichaitu123@gmail.com>
2025-04-23 12:05:52 +0530

    Created AllTasks to view all the tasks at bottom

---
+++ src/components/Others/AllTasks.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+const AllTasks = () => {
+  return (
+    <div className="bg-[#1c1c1c] p-5 rounded mt-5 h-48 overflow-auto">
+      <div className=" py-2 px-4 mb-2 flex justify-between bg-red-400 rounded">
+        <h2>Sarthak</h2>
+        <h3>Make a UI Design</h3>
+        <h5>Status</h5>
+      </div>
+      <div className=" py-2 px-4 mb-2 flex justify-between bg-blue-400 rounded">
+        <h2>Sarthak</h2>
+        <h3>Make a UI Design</h3>
+        <h5>Status</h5>
+      </div>
+      <div className=" py-2 px-4 mb-2 flex justify-between bg-green-400 rounded">
+        <h2>Sarthak</h2>
+        <h3>Make a UI Design</h3>
+        <h5>Status</h5>
+      </div>
+      <div className=" py-2 px-4 mb-2 flex justify-between bg-yellow-400 rounded">
+        <h2>Sarthak</h2>
+        <h3>Make a UI Design</h3>
+        <h5>Status</h5>
+      </div>
+      <div className=" py-2 px-4 mb-2 flex justify-between bg-blue-400 rounded">
+        <h2>Sarthak</h2>
+        <h3>Make a UI Design</h3>
+        <h5>Status</h5>
+      </div>
+      <div className=" py-2 px-4 mb-2 flex justify-between bg-red-400 rounded">
+        <h2>Sarthak</h2>
+        <h3>Make a UI Design</h3>
+        <h5>Status</h5>
+      </div>
+    </div>
+  );
+};
+
+export default AllTasks;
